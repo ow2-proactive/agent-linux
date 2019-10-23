de69765a91a2dc2dc3b99e4834702f1512ba87e7
Amine Mouhoub <ma.mouhoub@gmail.com>
2019-10-23 10:15:45 +0200

    Fix gems install path issue (#115)
    * Fix gems install path issue
    
    * Add force install option
    
    * Add verbose option to debug
    
    * Fix gems bin directory

--- packaging/build-packages.sh
+++ packaging/build-packages.sh
@@ -20,7 +20,7 @@ KIND=$1
 [ -n "$KIND" ] || ( echo "usage: $0 <deb|rpm>"; exit 1 )
 
 install_fpm () {
-    [ -f "$GEM_HOME/bin/fpm" ] || gem install fpm
+    [ -f "$GEM_HOME/bin/fpm" ] || gem install fpm -f -V --no-user-install --bindir $GEM_HOME/bin
 }
 
 package () {
